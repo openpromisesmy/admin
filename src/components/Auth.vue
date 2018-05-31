ab32c57883fa9a11b62ae521d213bfd893974d83
Nazreen <nxmohamad@gmail.com>
2018-06-01 02:16:55 +0700

    auth logic

---
+++ src/components/Auth.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="auth">
+      <template v-if="!authenticated">
+        <h1>Login or Sign Up</h1>
+        <el-button type="primary" v-on:click="googleSignInHandler">Google Sign In</el-button>
+      </template>
+  </main>
+</template>
+
+<script>
+// import { googleSignIn } from '@/api'
+function googleSignIn () {}
+
+export default {
+  name: 'Auth',
+  data () {
+    return {
+      authenticated: this.$store.state.user.authenticated
+    }
+  },
+  methods: {
+    googleSignInHandler: async function () {
+      try {
+        const user = await googleSignIn()
+        this.$store.commit('login', user)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,27 @@ import Home from '@/components/Home'
 import Promises from '@/components/Promises'
 import Politicians from '@/components/Politicians'
 import Contributors from '@/components/Contributors'
+import Auth from '@/components/Auth'
+
+import store from '../store'
 
 Vue.use(Router)
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.state.user.authenticated) {
+    next()
+    return
+  }
+  next('/')
+}
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.state.user.authenticated) {
+    next()
+    return
+  }
+  next('/login')
+}
 
 export default new Router({
   mode: 'history',
@@ -13,22 +32,32 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
+      beforeEnter: ifAuthenticated
     },
     {
       path: '/promises',
       name: 'Promises',
-      component: Promises
+      component: Promises,
+      beforeEnter: ifAuthenticated
     },
     {
       path: '/politicians',
       name: 'Politicians',
-      component: Politicians
+      component: Politicians,
+      beforeEnter: ifAuthenticated
     },
     {
       path: '/contributors',
       name: 'Contributors',
-      component: Contributors
+      component: Contributors,
+      beforeEnter: ifAuthenticated
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Auth,
+      beforeEnter: ifNotAuthenticated
     }
   ]
 })
