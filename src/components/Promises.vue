7df0c02dba9f1191d3f906d3515fe2a4c3e0e566
Nazreen <nxmohamad@gmail.com>
2018-06-01 01:32:05 +0700

    create Promises page

--- src/components/Promises.vue
+++ src/components/Promises.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <router-link to="/promises/">Promises</router-link>
+  <div>
+    <h1>Promises</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: 'Promises',
   data () {
     return {
       msg: ''
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Promises from '@/components/Promises'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/promises',
+      name: 'Promises',
+      component: Promises
     }
   ]
 })
