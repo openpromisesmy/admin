0f14f6d34893570007db0cc80f53d240dcf41340
Nazreen <nxmohamad@gmail.com>
2018-06-02 04:57:31 +0700

    list all promsies

--- src/components/Promises.vue
+++ src/components/Promises.vue
@@ -1,20 +1,93 @@
 <template>
   <div>
-    <h1>Promises</h1>
+    <h1>Promises {{ promises.length > 0 ? `- ${promises.length}` : 0 }}</h1>
+    <p v-if="promises.length === 0">Loading promises...</p>
+    <el-table
+    v-else
+    :data="promises"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="title"
+      label="Title"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="source_date"
+      label="Date"
+      width="150">
+    </el-table-column>
+    <el-table-column
+      prop="category"
+      label="Category"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="politician_name"
+      label="Politician"
+      width="150">
+    </el-table-column>
+    <!-- <el-table-column
+      prop="quote"
+      label="Quote"
+      width="250">
+    </el-table-column> -->
+    <el-table-column
+      prop="source_name"
+      label="Source">
+    </el-table-column>
+    <!-- <el-table-column
+      prop="source_url"
+      label="Source Link"
+      width="100">
+      <template slot-scope="scope">
+        <a :href="scope.row.source_url" target="_blank">View Source</a>
+      </template>
+    </el-table-column> -->
+    <el-table-column
+      prop="status"
+      label="Status"
+      width="125">
+    </el-table-column>
+  </el-table>
   </div>
 </template>
 
 <script>
 import { listPromises } from '@/api'
+import moment from 'moment'
+
 export default {
   name: 'Promises',
   data () {
     return {
-      promises: []
+      promises: [],
+      politicians: []
     }
   },
   async created () {
-    this.promises = await listPromises()
+    try {
+      const promises = await listPromises()
+      // const politicians = await listPoliticians()
+      // this.politicians = politicians
+      this.promises = this.parsePromises(promises/*, politicians */)
+      // this.promises = promises
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  methods: {
+    parsePromises (promises, politicians) {
+      return promises.map(promise =>
+        ({
+          ...promise,
+          status: promise.status ? promises.status : 'Review Needed',
+          source_date: moment(promise.source_date).format('D MMMM YYYY'),
+          politician_name: promise.politician_id
+          // politician_name: politicians.find(politician => politician.id === promise.politician_id).name
+        })
+      )
+    }
   }
 }
 </script>
