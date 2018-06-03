9c52bc5777f7fde825d4f7574714d217f11ada41
Nazreen <nxmohamad@gmail.com>
2018-06-04 05:54:04 +0800

    init component

---
+++ src/components/PromiseEditor.vue
@@ -0,0 +1,19 @@
+<template>
+  <main id="PromiseEditor">
+    <h1>Edit Promise</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'PromiseEditor',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
