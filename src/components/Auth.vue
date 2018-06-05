5e3847a25bebc406bb28ce4600cdae0eeb086857
Nazreen <nxmohamad@gmail.com>
2018-06-06 05:31:55 +0800

    show signing in

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,6 +1,9 @@
 <template>
   <main class="auth">
-      <template v-if="!authenticated">
+      <template v-if="appStatus === 'loading'">
+        <p>Signing in...</p>
+      </template>
+      <template v-else-if="!authenticated">
         <h1>Login or Sign Up</h1>
         <el-button type="primary" v-on:click="googleSignInHandler">Google Sign In</el-button>
       </template>
@@ -14,6 +17,7 @@ export default {
   name: 'Auth',
   data () {
     return {
+      appStatus: '',
       authenticated: this.$store.state.user.authenticated,
       status: this.$store.state.user.status
     }
@@ -22,7 +26,9 @@ export default {
     googleSignInHandler: async function () {
       try {
         const user = await googleSignIn()
+        this.appStatus = 'loading'
         const userData = await getContributor(user.email)
+        this.appStatus = ''
         this.$store.commit('login', Object.assign({}, user, userData[0]))
         this.$router.push('/home')
       } catch (e) {
