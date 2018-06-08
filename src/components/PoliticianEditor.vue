f17aa71e362abed01da65b00d0df4f730f88939f
Nazreen <nxmohamad@gmail.com>
2018-06-09 05:25:48 +0800

    fields and rules

---
+++ src/components/PoliticianEditor.vue
@@ -0,0 +1,129 @@
+<template>
+  <main id="PoliticianEditor">
+    <h1>{{ mode }} Politician</h1>
+    <template v-if="appStatus === 'loading'">
+      <p>Loading ...</p>
+    </template>
+    <template v-else-if="appStatus === 'submitting'">
+      <p>Submitting politician...</p>
+    </template>
+    <template v-else-if="appStatus === 'submitted'">
+      <p>Politician has been updated</p>
+    </template>
+    <el-form v-else v-on:submit.prevent="onSubmit" :rules="rules" label-position="left" label-width="100px" ref="form" :model="politician">
+        <el-row >
+
+        <el-col :xs="24" :sm="12" >
+              <el-form-item label="Live" prop="live">
+            <el-select v-model="politician.live" placeholder="Select">
+              <el-option
+                v-for="item in liveOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
+              </el-form-item>
+          </el-col>
+
+          <el-col :xs="24" :sm="24" >
+              <el-form-item label="Name" prop="name">
+            <el-input type="text" placeholder="enter name" v-model="politician.name"></el-input>
+              </el-form-item>
+          </el-col>
+
+          <el-col :xs="24" :sm="24" >
+              <el-form-item label="Primary Position" prop="primary_position">
+            <el-input type="text" placeholder="enter primary position" v-model="politician.primary_position"></el-input>
+              </el-form-item>
+          </el-col>
+
+
+          <el-col :xs="24" :sm="12" >
+              <el-form-item label="Status" prop="stats">
+            <el-input type="text" placeholder="enter status" v-model="politician.status"></el-input>
+              </el-form-item>
+          </el-col>
+
+          <el-col :xs="24" :sm="12" >
+              <el-form-item label="Profile Image" prop="profile_image">
+            <el-input type="text" placeholder="enter profile image link" v-model="politician.profile"></el-input>
+              </el-form-item>
+          </el-col>
+
+        </el-row>
+
+          <el-button v-on:click="onSubmit"> Submit </el-button>
+        </el-form>
+  </main>
+</template>
+
+<script>
+import { getPolitician, listPoliticians, listContributors, updatePolitician } from '@/api'
+export default {
+  name: 'PoliticianEditor',
+  data () {
+    return {
+      appStatus: 'loading',
+      mode: '',
+      liveOptions: [{ label: 'true', value: true }, { label: 'false', value: false}], 
+      politician: {
+
+      },
+      rules: {
+        live: [{ required: true, type: 'boolean', message: 'Please select whether politician is live.', trigger: 'blur' }],
+        name: [{ required: true, message: 'Please input politician name', trigger: 'blur' }],
+        // contributor_id: [{ required: true, type: 'text', message: 'contributor_id is required', trigger: 'blur' }],
+        primary_position: [{ required: true, message: 'primary position is required', trigger: 'blur' }],
+        status: [{ required: true, message: 'Please enter status', trigger: 'blur' }],
+        profile_image: [{ required: true, type: 'url', message: 'Please indicate the profile image url', trigger: 'blur' }]
+      }
+    }
+  },
+  computed: {
+    contributor: function () {
+      if (this.contributors.length > 0) {
+        const contributor = this.contributors.find(contributor => contributor.id === this.politician.contributor_id)
+        if (!contributor) return
+        return contributor.name + ' - ' + contributor.email
+      }
+    }
+  },
+  async created () {
+    try {
+      this.mode = this.$route.path.split('/').slice(-1)[0]
+      this.appStatus = ''
+    } catch (e) {
+      console.error(e)
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          this.submitPolitician(this.politician)
+        } else {
+          return false
+        }
+      })
+    },
+    async submitPolitician (politician) {
+      try {
+        this.appStatus = 'submitting'
+        delete politician.contributor_id
+        await updatePolitician(politician)
+        this.appStatus = 'submitted'
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#PoliticianEditor {
+  max-width: 900px
+}
+
+</style>
