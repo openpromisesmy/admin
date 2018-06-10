d94a85c6d93c480e44bd13eb0b2b105db1c7c452
Nazreen <nxmohamad@gmail.com>
2018-06-11 04:44:36 +0800

    create filterByStatus

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,11 @@ function formatDate (date) {
   return moment(date).format('D MMM YYYY')
 }
 
+function filterByStatus (promises, status) {
+  return promises.filter(promise => promise.status === status)
+}
+
 export {
-  formatDate
+  formatDate,
+  filterByStatus
 }
