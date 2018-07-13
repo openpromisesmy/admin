a717ad02672653da89a26f0c7eec3fb35b1d43bd
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-13 16:59:33 +0800

    get all promises including not live

--- src/api/index.js
+++ src/api/index.js
@@ -130,7 +130,7 @@ const postPromise = data => postSomething(PROMISES_PATH, data)
 const postPolitician = data => postSomething(POLITICIANS_PATH, data)
 const listPoliticians = () => getSomething(POLITICIANS_PATH + 'all')
 const listPromises = query => getSomething(PROMISES_PATH + 'all?' + query)
-const listPoliticianPromises = id => getSomething(PROMISES_PATH + '?politician_id=' + id)
+const listPoliticianPromises = id => getSomething(PROMISES_PATH + 'all?politician_id=' + id)
 
 const updatePromise = data => updateSomething(`${PROMISES_PATH}${data.id}`, data)
 const updatePolitician = data => updateSomething(`${POLITICIANS_PATH}${data.id}`, data)
