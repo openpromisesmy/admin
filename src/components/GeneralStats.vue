21a2be469c85fd3aca200dda45f796a01ce63355
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-21 01:36:25 +0800

    general stats

---
+++ src/components/GeneralStats.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-card id="shortcuts">
+        <div slot="header" class="clearfix">
+          <b>General Stats</b>
+        </div>
+          <b>Politicians</b>
+          <p v-for="(value, key) in generalStats.politicians" :key="key">
+              {{ key }}: {{ value }}
+          </p>
+          <b>Promises</b>
+            <pre>{{ JSON.stringify(generalStats.promises, null, 2) }}</pre>
+          <p>by status:</p>
+          <p v-for="(value, key) in generalStats.promises.countByStatus" :key="key">
+              {{ key }}: {{ value }}
+          </p>
+    </el-card>
+</template>
+
+<script>
+export default {
+  name: 'GeneralStats',
+  props: ['generalStats']
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -31,21 +31,25 @@
               </a>
             </el-row>
     </el-card>
+    <general-stats v-bind="{ generalStats }" />
   </div>
 </template>
 
 <script>
 import { getGeneralStats } from '@/api'
+import GeneralStats from '@/components/GeneralStats'
+
 export default {
   name: 'Home',
+  components: { GeneralStats },
   data () {
     return {
-      stats: {}
+      generalStats: {}
     }
   },
   async created () {
     try {
-    this.stats = await getGeneralStats()
+      this.generalStats = await getGeneralStats()
     } catch (e) {
       alert(e)
     }
