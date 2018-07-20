92fd6fa9e7ab91dbccf48804211ea7110061a66b
Nazreen <nxmohamad@gmail.com>
2018-07-21 01:22:42 +0800

    feat(Home): update layout (#55)
    * add slogan
    
    * one per line
    
    * descriptions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="home">
-    <h1>OpenPromises Admin Dashboard</h1>
-    <router-link to="/promises">
-      <el-button type="default"> Promises </el-button>
-    </router-link>
-    <router-link to="/politicians">
-      <el-button type="default"> Politicians </el-button>
-    </router-link>
-    <router-link to="/contributors">
-      <el-button type="default"> Contributors </el-button>
-    </router-link>
+    <h1>OpenPromises Tracking Dashboard</h1>
+    <h2 id="slogan">"Shifting Malaysia's political culture, one promise at a time."</h2>
+    <el-card id="shortcuts">
+        <div slot="header" class="clearfix">
+          <b>Shortcuts to make your life easier</b>
+        </div>
+            <el-row>
+              <span>Ensuring words are kept: </span>
+              <router-link to="/promises/new">
+                <el-button type="primary"> New Promise </el-button>
+              </router-link>
+            </el-row>
+            <el-row>
+              <span>With power, comes responsibility: </span>
+              <router-link to="/politicians">
+                <el-button type="primary"> New Politician </el-button>
+              </router-link>
+            </el-row>
+            <el-row>
+              <span>Helping Malaysians stay up to date: </span>
+              <a href="https://www.facebook.com/openpromises.malaysia/" target="_blank">
+                  <el-button type="default"> Facebook Page </el-button>
+              </a>
+            </el-row>
+            <el-row>
+              <span>Curating constructive discussions: </span>
+              <a href="https://www.facebook.com/groups/1809117962724326/" target="_blank">
+                  <el-button type="default"> Watchers Group </el-button>
+              </a>
+            </el-row>
+    </el-card>
   </div>
 </template>
 
@@ -26,4 +47,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#slogan {
+  font-weight: 300
+}
+
+#shortcuts {
+  max-width: 900px;
+  margin: 20px auto
+}
+
+.el-card .el-row {
+  margin-bottom: 10px
+}
 </style>
