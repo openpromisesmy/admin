eefeb4a39dc421f9ebc2ba09838457dba4c82a27
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-21 01:16:39 +0800

    one per line

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,15 +2,31 @@
   <div class="home">
     <h1>OpenPromises Tracking Dashboard</h1>
     <h2 id="slogan">"Shifting Malaysia's political culture, one promise at a time."</h2>
-    <router-link to="/promises">
-      <el-button type="default"> Promises </el-button>
-    </router-link>
-    <router-link to="/politicians">
-      <el-button type="default"> Politicians </el-button>
-    </router-link>
-    <router-link to="/contributors">
-      <el-button type="default"> Contributors </el-button>
-    </router-link>
+    <el-card id="shortcuts">
+        <div slot="header" class="clearfix">
+          <b>Shortcuts to make your life easier</b>
+        </div>
+            <el-row>
+              <router-link to="/promises/new">
+                <el-button type="primary"> New Promise </el-button>
+              </router-link>
+            </el-row>
+            <el-row>
+              <router-link to="/politicians">
+                <el-button type="primary"> New Politician </el-button>
+              </router-link>
+            </el-row>
+            <el-row>
+              <a href="https://www.facebook.com/openpromises.malaysia/" target="_blank">
+                  <el-button type="default"> Facebook Page </el-button>
+              </a>
+            </el-row>
+            <el-row>
+              <a href="https://www.facebook.com/groups/1809117962724326/" target="_blank">
+                  <el-button type="default"> Watchers Group </el-button>
+              </a>
+            </el-row>
+    </el-card>
   </div>
 </template>
 
@@ -29,5 +45,14 @@ export default {
 <style scoped>
 #slogan {
   font-weight: 300
+}
+
+#shortcuts {
+  max-width: 900px;
+  margin: 20px auto
+}
+
+.el-card .el-row {
+  margin-bottom: 10px
 }
 </style>
