3824707e76a3d5240e48c7afbde4c8027272e87f
Nazreen Mohamad <nxmohamad@gmail.com>
2018-07-23 01:08:48 +0800

    edit layout

--- src/components/PromiseStatsWidget.vue
+++ src/components/PromiseStatsWidget.vue
@@ -2,16 +2,25 @@
   <el-card id="PromiseStats">
     <b>Total Promises Made: {{ promises.length }}</b>
     <div v-for="stat in stats" :key="stat.value">
-      <span>{{ stat.value }}</span>
-      <vue-slide-bar
-        v-model="stat.number"
-        :max="promises.length"
-        :processStyle="sliderCustomzie.processStyle"
-        :lineHeight="sliderCustomzie.lineHeight"
-        :tooltipStyles="sliderCustomzie.tooltipStyles"
-        :isDisabled="true"
-      />
-      <span>{{ promises.length }}</span>
+      <el-row>
+        <el-col :span="8">
+          <b>{{ stat.value }}</b>
+        </el-col>
+        <el-col :span="8">
+          <vue-slide-bar
+            v-model="stat.number"
+            :max="promises.length"
+            :processStyle="sliderCustomzie.processStyle"
+            :lineHeight="sliderCustomzie.lineHeight"
+            :tooltipStyles="sliderCustomzie.tooltipStyles"
+            :isDisabled="true"
+            :show-tooltip="false"
+          />
+        </el-col>
+        <el-col :span="8">
+          <span><b>{{ stat.number }}</b> out of<b> {{ promises.length }}</b></span>
+        </el-col>
+      </el-row>
     </div>
   </el-card>
 </template>
@@ -35,7 +44,8 @@ export default {
         // val: 9,
         lineHeight: 20,
         processStyle: {
-          backgroundColor: '#42b883'
+          backgroundColor: '#42b883',
+          borderRadius: '2px'
         },
         tooltipStyles: {
           backgroundColor: '#42b883',
@@ -50,7 +60,19 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #PromiseStats {
-  max-width: 500px;
+  max-width: 600px;
   margin: 0 auto
+}
+
+.vue-slide-bar-component {
+  padding-top: 0px !important
+}
+
+.vue-slide-bar {
+  border-radius: none !important
+}
+
+.el-row {
+  margin: 10px auto
 }
 </style>
