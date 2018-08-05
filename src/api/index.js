7bae2c790cdaf04446068a4f3a32ce8b1759fed0
Nazreen Mohamad <nxmohamad@gmail.com>
2018-08-06 00:24:21 +0800

    extract utils

--- src/api/index.js
+++ src/api/index.js
@@ -1,35 +1,16 @@
-import { firebase, API_URL } from '@/config'
+import { firebase } from '@/config'
 import axios from 'axios'
+import {
+  getSomething,
+  postSomething,
+  updateSomething
+} from './utils'
+
 const provider = new firebase.auth.GoogleAuthProvider()
 
 const PROMISES_PATH = '/promises/'
 const PROMISE_UPDATES_PATH = '/promiseUpdates/'
 const POLITICIANS_PATH = '/politicians/'
-
-async function setAuthData () {
-  return new Promise((resolve, reject) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        axios.interceptors.request.use(
-          async function (config) {
-            const email = localStorage.getItem('openpromises_email')
-            // if (firebase.auth().currentUser) {
-            config.headers['X-FIREBASE-TOKEN'] = await firebase.auth().currentUser.getIdToken()
-            config.headers['X-USER-EMAIL'] = email
-            // }
-            return config
-          },
-          function (error) {
-            return Promise.reject(error)
-          }
-        )
-        resolve(user)
-      } else {
-        // No user is signed in.
-      }
-    })
-  })
-}
 
 axios.interceptors.response.use(
   function (response) {
@@ -87,39 +68,6 @@ function googleLogout () {
       reject(error)
     })
   })
-}
-
-async function getSomething (path) {
-  try {
-    await setAuthData()
-    const response = await axios.get(API_URL + path)
-    return response.data
-  } catch (e) {
-    console.error(e)
-  }
-}
-
-async function postSomething (path, data) {
-  try {
-    const response = await axios.post(API_URL + path, data)
-    return response.data
-  } catch (e) {
-    console.error(e)
-    return e
-  }
-}
-
-async function updateSomething (path, data) {
-  try {
-    delete data.id
-    delete data.created_at
-    delete data.updated_at
-    const response = await axios.post(API_URL + path, data)
-    return response.data
-  } catch (e) {
-    console.error(e)
-    return e
-  }
 }
 
 const getContributor = email => getSomething(`/contributors/?email=${email}`)
