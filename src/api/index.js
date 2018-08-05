5daac9eaff0b051c6871041aed667c26a2aaa294
Nazreen <nxmohamad@gmail.com>
2018-08-06 00:29:56 +0800

    refactor(api): separate into multiple files (#67)
    * extract utils
    
    * extract google api calls
    
    * reorder

--- src/api/index.js
+++ src/api/index.js
@@ -1,35 +1,17 @@
-import { firebase, API_URL } from '@/config'
 import axios from 'axios'
-const provider = new firebase.auth.GoogleAuthProvider()
+import {
+  getSomething,
+  postSomething,
+  updateSomething
+} from './utils'
+import {
+  googleSignIn,
+  googleLogout
+} from './google'
 
 const PROMISES_PATH = '/promises/'
 const PROMISE_UPDATES_PATH = '/promiseUpdates/'
 const POLITICIANS_PATH = '/politicians/'
-
-async function setAuthData () {
-  return new Promise((resolve, reject) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        axios.interceptors.request.use(
-          async function (config) {
-            const email = localStorage.getItem('openpromises_email')
-            // if (firebase.auth().currentUser) {
-            config.headers['X-FIREBASE-TOKEN'] = await firebase.auth().currentUser.getIdToken()
-            config.headers['X-USER-EMAIL'] = email
-            // }
-            return config
-          },
-          function (error) {
-            return Promise.reject(error)
-          }
-        )
-        resolve(user)
-      } else {
-        // No user is signed in.
-      }
-    })
-  })
-}
 
 axios.interceptors.response.use(
   function (response) {
@@ -44,103 +26,25 @@ axios.interceptors.response.use(
     return Promise.reject(error)
   })
 
-function googleSignIn () {
-  return new Promise((resolve, reject) => {
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(result => {
-        const raw = result.user
-        let user = {}
-
-        user.name = raw.displayName
-        user.email = raw.email
-        user.photoURL = raw.photoURL
-
-        firebase
-          .auth()
-          .currentUser.getIdToken(/* forceRefresh */ true)
-          .then(idToken => {
-            user.token = idToken
-            localStorage.setItem('openpromises_token', idToken)
-            localStorage.setItem('openpromises_email', user.email)
-            localStorage.setItem('openpromises_name', user.name)
-            localStorage.setItem('openpromises_photo', user.photoURL)
-            resolve(user)
-          })
-          .catch(error => {
-            reject(error)
-          })
-      })
-      .catch(error => {
-        console.error(error)
-        reject(error)
-      })
-  })
-}
-
-function googleLogout () {
-  return new Promise((resolve, reject) => {
-    firebase.auth().signOut().then(function () {
-      resolve()
-    }, function (error) {
-      reject(error)
-    })
-  })
-}
-
-async function getSomething (path) {
-  try {
-    await setAuthData()
-    const response = await axios.get(API_URL + path)
-    return response.data
-  } catch (e) {
-    console.error(e)
-  }
-}
-
-async function postSomething (path, data) {
-  try {
-    const response = await axios.post(API_URL + path, data)
-    return response.data
-  } catch (e) {
-    console.error(e)
-    return e
-  }
-}
-
-async function updateSomething (path, data) {
-  try {
-    delete data.id
-    delete data.created_at
-    delete data.updated_at
-    const response = await axios.post(API_URL + path, data)
-    return response.data
-  } catch (e) {
-    console.error(e)
-    return e
-  }
-}
-
-const getContributor = email => getSomething(`/contributors/?email=${email}`)
-const getPolitician = id => getSomething(`/politicians/${id}`)
-const getPromise = id => getSomething(PROMISES_PATH + id)
-const getPromiseUpdate = id => getSomething(PROMISE_UPDATES_PATH + id)
-
 const listContributors = () => getSomething('/contributors/')
-
-const postPromise = data => postSomething(PROMISES_PATH, data)
-const postPromiseUpdate = data => postSomething(PROMISE_UPDATES_PATH, data)
-const postPolitician = data => postSomething(POLITICIANS_PATH, data)
+const getContributor = email => getSomething(`/contributors/?email=${email}`)
 
 const listPoliticians = () => getSomething(POLITICIANS_PATH + 'all?orderBy=name')
-const listPromises = query => getSomething(PROMISES_PATH + 'all?' + query)
-const listPromiseUpdates = query => getSomething(PROMISE_UPDATES_PATH)
+const getPolitician = id => getSomething(`/politicians/${id}`)
+const postPolitician = data => postSomething(POLITICIANS_PATH, data)
+const updatePolitician = data => updateSomething(`${POLITICIANS_PATH}${data.id}`, data)
+
 const listPoliticianPromises = id => getSomething(PROMISES_PATH + 'all?politician_id=' + id)
 
+const listPromises = query => getSomething(PROMISES_PATH + 'all?' + query)
+const getPromise = id => getSomething(PROMISES_PATH + id)
+const postPromise = data => postSomething(PROMISES_PATH, data)
 const updatePromise = data => updateSomething(`${PROMISES_PATH}${data.id}`, data)
+
+const listPromiseUpdates = query => getSomething(PROMISE_UPDATES_PATH)
+const getPromiseUpdate = id => getSomething(PROMISE_UPDATES_PATH + id)
+const postPromiseUpdate = data => postSomething(PROMISE_UPDATES_PATH, data)
 const updatePromiseUpdate = data => updateSomething(`${PROMISE_UPDATES_PATH}${data.id}`, data)
-const updatePolitician = data => updateSomething(`${POLITICIANS_PATH}${data.id}`, data)
 
 const getGeneralStats = () => getSomething('/stats/general_stats')
 
---
+++ src/api/utils.js
@@ -0,0 +1,66 @@
+import { firebase, API_URL } from '@/config'
+import axios from 'axios'
+
+async function setAuthData () {
+  return new Promise((resolve, reject) => {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        axios.interceptors.request.use(
+          async function (config) {
+            const email = localStorage.getItem('openpromises_email')
+            // if (firebase.auth().currentUser) {
+            config.headers['X-FIREBASE-TOKEN'] = await firebase.auth().currentUser.getIdToken()
+            config.headers['X-USER-EMAIL'] = email
+            // }
+            return config
+          },
+          function (error) {
+            return Promise.reject(error)
+          }
+        )
+        resolve(user)
+      } else {
+        // No user is signed in.
+      }
+    })
+  })
+}
+
+async function getSomething (path) {
+  try {
+    await setAuthData()
+    const response = await axios.get(API_URL + path)
+    return response.data
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+async function postSomething (path, data) {
+  try {
+    const response = await axios.post(API_URL + path, data)
+    return response.data
+  } catch (e) {
+    console.error(e)
+    return e
+  }
+}
+
+async function updateSomething (path, data) {
+  try {
+    delete data.id
+    delete data.created_at
+    delete data.updated_at
+    const response = await axios.post(API_URL + path, data)
+    return response.data
+  } catch (e) {
+    console.error(e)
+    return e
+  }
+}
+
+export {
+  getSomething,
+  postSomething,
+  updateSomething
+}
