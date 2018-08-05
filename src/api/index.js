5a41d71f3bdb1ce1ccf725a7bfd01943f7dc2e0d
Nazreen Mohamad <nxmohamad@gmail.com>
2018-08-06 00:28:26 +0800

    reorder

--- src/api/index.js
+++ src/api/index.js
@@ -26,25 +26,25 @@ axios.interceptors.response.use(
     return Promise.reject(error)
   })
 
-const getContributor = email => getSomething(`/contributors/?email=${email}`)
-const getPolitician = id => getSomething(`/politicians/${id}`)
-const getPromise = id => getSomething(PROMISES_PATH + id)
-const getPromiseUpdate = id => getSomething(PROMISE_UPDATES_PATH + id)
-
 const listContributors = () => getSomething('/contributors/')
-
-const postPromise = data => postSomething(PROMISES_PATH, data)
-const postPromiseUpdate = data => postSomething(PROMISE_UPDATES_PATH, data)
-const postPolitician = data => postSomething(POLITICIANS_PATH, data)
+const getContributor = email => getSomething(`/contributors/?email=${email}`)
 
 const listPoliticians = () => getSomething(POLITICIANS_PATH + 'all?orderBy=name')
-const listPromises = query => getSomething(PROMISES_PATH + 'all?' + query)
-const listPromiseUpdates = query => getSomething(PROMISE_UPDATES_PATH)
+const getPolitician = id => getSomething(`/politicians/${id}`)
+const postPolitician = data => postSomething(POLITICIANS_PATH, data)
+const updatePolitician = data => updateSomething(`${POLITICIANS_PATH}${data.id}`, data)
+
 const listPoliticianPromises = id => getSomething(PROMISES_PATH + 'all?politician_id=' + id)
 
+const listPromises = query => getSomething(PROMISES_PATH + 'all?' + query)
+const getPromise = id => getSomething(PROMISES_PATH + id)
+const postPromise = data => postSomething(PROMISES_PATH, data)
 const updatePromise = data => updateSomething(`${PROMISES_PATH}${data.id}`, data)
+
+const listPromiseUpdates = query => getSomething(PROMISE_UPDATES_PATH)
+const getPromiseUpdate = id => getSomething(PROMISE_UPDATES_PATH + id)
+const postPromiseUpdate = data => postSomething(PROMISE_UPDATES_PATH, data)
 const updatePromiseUpdate = data => updateSomething(`${PROMISE_UPDATES_PATH}${data.id}`, data)
-const updatePolitician = data => updateSomething(`${POLITICIANS_PATH}${data.id}`, data)
 
 const getGeneralStats = () => getSomething('/stats/general_stats')
 
