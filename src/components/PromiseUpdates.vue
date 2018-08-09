be9c4d3839dc5c4b5d953b7235a650aea98cc169
Nazreen Mohamad <nxmohamad@gmail.com>
2018-08-09 22:54:14 +0800

    format

--- src/components/PromiseUpdates.vue
+++ src/components/PromiseUpdates.vue
@@ -1,8 +1,18 @@
 <template>
   <main class="account">
-      <el-card v-for="promiseUpdate in promiseUpdates" :key="promiseUpdate.id">
-        <p v-for="(value,key) in promiseUpdate" :key="key"><b>{{ key }}</b>: {{ value }}</p>
+      <el-card
+        v-for="promiseUpdate in promiseUpdates"
+        :key="promiseUpdate.id"
+        v-if="promiseUpdates.length > 0"
+      >
+        <p
+          v-for="(value,key) in promiseUpdate"
+          :key="key"
+        >
+          <b>{{ key }}</b>: {{ value }}
+        </p>
       </el-card>
+      <p v-else>No promise updates.</p>
   </main>
 </template>
 
