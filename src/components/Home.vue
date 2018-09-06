f5385fa7c8f5c4b243a236ca37e0f2adf89f4068
Nazreen Mohamad <nxmohamad@gmail.com>
2018-09-07 03:51:39 +0800

    update shortcuts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,26 +2,36 @@
   <div class="home container">
     <h1>OpenPromises Tracking Dashboard</h1>
     <h2 id="slogan">"Shifting Malaysia's political culture, one promise at a time."</h2>
-    <el-card id="shortcuts">
-        <div slot="header" class="clearfix">
-          <b>Shortcuts</b>
-        </div>
             <el-row>
-              <router-link to="/promises/new">
-                <el-button type="primary"> New Promise </el-button>
-              </router-link>
-              <router-link to="/politicians">
-                <el-button type="primary"> New Politician </el-button>
-              </router-link>
-              <a href="https://www.facebook.com/openpromises.malaysia/" target="_blank">
-                  <el-button type="default"> Facebook Page </el-button>
-              </a>
-              <a href="https://www.facebook.com/groups/1809117962724326/" target="_blank">
-                  <el-button type="default"> Watchers Group </el-button>
-              </a>
+              <el-col :span="12">
+                <el-card id="shortcuts">
+                <div slot="header" class="clearfix">
+                  <b>Add New Shortcuts</b>
+                </div>
+                <router-link to="/promises/new">
+                  <el-button type="primary"> New Promise </el-button>
+                </router-link>
+                <router-link to="/politicians">
+                  <el-button type="primary"> New Politician </el-button>
+                </router-link>
+                </el-card>
+              </el-col>
+
+              <el-col :span="12">
+                <el-card id="shortcuts">
+                  <div slot="header" class="clearfix">
+                    <b>External Shortcuts</b>
+                  </div>
+                  <a href="https://www.facebook.com/openpromises.malaysia/" target="_blank">
+                      <el-button type="default"> Facebook Page </el-button>
+                  </a>
+                  <a href="https://www.facebook.com/groups/1809117962724326/" target="_blank">
+                      <el-button type="default"> Watchers Group </el-button>
+                  </a>
+                </el-card>
+              </el-col>
             </el-row>
-    </el-card>
-  </div>
+    </div>
 </template>
 
 <script>
