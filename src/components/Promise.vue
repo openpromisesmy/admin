206ecd2d3d4ec3a6a8d4809ad95d244c07653188
Nazreen Mohamad <nxmohamad@gmail.com>
2018-10-28 01:34:35 +0700

    lint error

--- src/components/Promise.vue
+++ src/components/Promise.vue
@@ -32,7 +32,7 @@
       <el-card v-if="promise.cover_image" class="Promise_card_image">
         <p class="card-title"> <b> Image </b></p>
         <img :src="promise.cover_image" />
-      </el-card> 
+      </el-card>
 
       <router-link :to="'/promises/' + promise.id + '/edit'">
         <el-button type="success" class="edit-button"> Edit Promise </el-button>
