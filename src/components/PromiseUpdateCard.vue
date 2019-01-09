49a837e40121dfd6f020470308e0490025a33d31
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-10 05:21:35 +0800

    display contributor email

--- src/components/PromiseUpdateCard.vue
+++ src/components/PromiseUpdateCard.vue
@@ -4,6 +4,7 @@
       <div slot="header">
         <el-tag class="status">{{ promiseUpdate.status }}</el-tag>
         {{ formatDate(promiseUpdate.source_date) }}
+        <span v-if="contributor">{{ contributor.email }}</span>
         <span class="source">
           Source:
           <a :href="promiseUpdate.source_url">
@@ -26,9 +27,14 @@ import { formatDate } from '@/utils'
 // TODO: color code for the status tags
 export default {
   name: 'PromiseUpdateCard',
-  props: ['promiseUpdate'],
+  props: ['promiseUpdate', 'contributors'],
   methods: {
     formatDate
+  },
+  computed: {
+    contributor () {
+      return this.contributors.find(x => x.id === this.promiseUpdate.contributor_id)
+    }
   }
 }
 </script>
