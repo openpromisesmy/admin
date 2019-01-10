6f969626898b8f1efa957f6ed007568531d27669
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-11 00:24:42 +0800

    setup

--- src/main.js
+++ src/main.js
@@ -8,9 +8,13 @@ import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 import './index.css'
+import VueIziToast from 'vue-izitoast'
+
+import 'izitoast/dist/css/iziToast.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, { locale })
+Vue.use(VueIziToast)
 
 /* eslint-disable no-new */
 new Vue({
