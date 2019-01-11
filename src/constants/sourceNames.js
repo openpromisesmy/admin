f62ae1159db7a1d149170808aae966e759f903dc
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-12 06:31:26 +0800

    update value

--- src/constants/sourceNames.js
+++ src/constants/sourceNames.js
@@ -1,15 +1,14 @@
 export default [
-  'The Star',
-  'Malay Mail',
-  'PH Manifesto GE14',
-  'MalaysiaKini',
-  'The Malaysian Insight',
-  'New Straits Times',
-  'The Sun',
-  'Utusan Borneo',
-  'The Borneo Post',
-  'Daily Express',
-  'Official Twitter',
-  'Official Facebook',
-  'Others'
+  { name: 'The Star', hostname: 'www.thestar.com.my' },
+  { name: 'Malay Mail', hostname: 'www.malaymail.com' },
+  { name: 'PH Manifesto GE14', hostname: 'pakatanharapan.com.my' },
+  { name: 'MalaysiaKini', hostname: 'www.malaysiakini.com' },
+  { name: 'The Malaysian Insight', hostname: 'www.themalaysianinsight.com' },
+  { name: 'New Straits Times', hostname: 'www.nst.com.my' },
+  { name: 'The Sun', hostname: 'www.thesundaily.my' },
+  { name: 'Utusan Borneo', hostname: 'www.utusanborneo.com.my' },
+  { name: 'The Borneo Post', hostname: 'www.theborneopost.com' },
+  { name: 'Daily Express', hostname: 'www.dailyexpress.com.my' },
+  { name: 'Official Facebook', hostname: 'www.facebook.com' },
+  { name: 'Official Twitter', hostname: 'www.twitter.com' }
 ]
