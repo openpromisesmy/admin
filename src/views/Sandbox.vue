6fba55f3fdff864e0aa88fc8b5b4e3e2269cac8d
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-12 02:43:18 +0800

    init component

---
+++ src/views/Sandbox.vue
@@ -0,0 +1,8 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
