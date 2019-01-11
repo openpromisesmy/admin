30d0c77a0c5113811c389ad5ece0d401ab1f38ca
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-12 02:56:11 +0800

    setup

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,8 +1,38 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
-    <el-aside width="200px">Aside</el-aside>
-    <el-main>Main</el-main>
+    <el-header height="150px">
+      <h1>Sandbox</h1>
+      <p>Use this for building new components in isolation before importing them into their parent components.</p>
+    </el-header>
+    <el-main>
+      <div id="test"/>
+    </el-main>
     <el-footer>Footer</el-footer>
   </el-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.el-header,
+.el-footer,
+.el-aside {
+  text-align: center;
+}
+
+.el-main {
+  background-color: black;
+}
+
+#test {
+  height: 100px;
+  width: 100px;
+  background-color: red;
+}
+</style>
