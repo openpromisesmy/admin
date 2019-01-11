d55825a25e76af6ba666c03d8a7e5e487066dc28
Nazreen <nxmohamad@gmail.com>
2019-01-12 02:56:39 +0800

    feat(Sandbox): setup (#127)
    * init component
    
    * add to route when in dev mode
    
    * setup

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import PoliticianEditor from '@/components/PoliticianEditor'
 import Stats from '@/views/Stats'
 import Promise from '@/views/Promise'
 import ReviewNeeded from '@/views/ReviewNeeded'
+import Sandbox from '@/views/Sandbox'
 
 import store from '../store'
 
@@ -33,92 +34,102 @@ const ifAuthenticated = (to, from, next) => {
   next('/login')
 }
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/promises',
+    name: 'Promises',
+    component: Promises,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/politicians',
+    name: 'Politicians',
+    component: Politicians,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/contributors',
+    name: 'Contributors',
+    component: Contributors,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: Account,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Auth,
+    beforeEnter: ifNotAuthenticated
+  },
+  {
+    path: '/promises/review-needed',
+    name: 'Review Needed',
+    component: ReviewNeeded,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/promises/new',
+    name: 'New Promise',
+    component: PromiseEditor,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/promises/:id',
+    name: 'Promise',
+    component: Promise,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/promises/:id/edit',
+    name: 'Edit Promise',
+    component: PromiseEditor,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/politicians/new',
+    name: 'New Politician',
+    component: PoliticianEditor,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/politicians/:id',
+    name: 'Politician',
+    component: Politician,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/politicians/:id/edit',
+    name: 'Edit Politician',
+    component: PoliticianEditor,
+    beforeEnter: ifAuthenticated
+  },
+  {
+    path: '/stats',
+    name: 'Stats',
+    component: Stats,
+    beforeEnter: ifAuthenticated
+  }
+]
+
+if (process.env.NODE_ENV === 'development') {
+  routes.push({
+    path: '/sandbox',
+    name: 'Sandbox',
+    component: Sandbox
+  })
+}
+
 export default new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/promises',
-      name: 'Promises',
-      component: Promises,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/politicians',
-      name: 'Politicians',
-      component: Politicians,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/contributors',
-      name: 'Contributors',
-      component: Contributors,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/account',
-      name: 'Account',
-      component: Account,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Auth,
-      beforeEnter: ifNotAuthenticated
-    },
-    {
-      path: '/promises/review-needed',
-      name: 'Review Needed',
-      component: ReviewNeeded,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/promises/new',
-      name: 'New Promise',
-      component: PromiseEditor,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/promises/:id',
-      name: 'Promise',
-      component: Promise,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/promises/:id/edit',
-      name: 'Edit Promise',
-      component: PromiseEditor,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/politicians/new',
-      name: 'New Politician',
-      component: PoliticianEditor,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/politicians/:id',
-      name: 'Politician',
-      component: Politician,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/politicians/:id/edit',
-      name: 'Edit Politician',
-      component: PoliticianEditor,
-      beforeEnter: ifAuthenticated
-    },
-    {
-      path: '/stats',
-      name: 'Stats',
-      component: Stats,
-      beforeEnter: ifAuthenticated
-    }
-  ]
+  routes
 })
---
+++ src/views/Sandbox.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-container>
+    <el-header height="150px">
+      <h1>Sandbox</h1>
+      <p>Use this for building new components in isolation before importing them into their parent components.</p>
+    </el-header>
+    <el-main>
+      <div id="test"/>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.el-header,
+.el-footer,
+.el-aside {
+  text-align: center;
+}
+
+.el-main {
+  background-color: black;
+}
+
+#test {
+  height: 100px;
+  width: 100px;
+  background-color: red;
+}
+</style>
