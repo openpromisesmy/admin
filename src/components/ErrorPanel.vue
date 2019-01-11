a1145a46b57cadc1acb922d1b3714208417aad81
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-12 03:01:50 +0800

    create ErrorPanel

---
+++ src/components/ErrorPanel.vue
@@ -0,0 +1,21 @@
+<template>
+   <el-alert
+    :title="message"
+    type="error">
+  </el-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: () => {}
+    },
+    messageArray: {
+      type: Array,
+      default: () => {}
+    }
+  }
+}
+</script>
