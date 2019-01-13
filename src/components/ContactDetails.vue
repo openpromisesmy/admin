8b5c73f9aded4ed2dd01f0647d585b617a071835
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-14 04:05:55 +0800

    header

--- src/components/ContactDetails.vue
+++ src/components/ContactDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card header="Contact Details">
     <p><b>Email:</b> {{ email }} </p>
     <p><b>Phone Number:</b> {{ phone_number }} </p>
     <p><b>Facebook:</b> <a :href="facebook_url"> {{ facebook_url }} </a> </p>
