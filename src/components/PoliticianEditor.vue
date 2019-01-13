1489c363113e52840fd0136cb4f9602fa9833cb0
Nazreen <nxmohamad@gmail.com>
2019-01-14 03:52:19 +0800

    feat(PoliticianEditor): include contact details (#136)
    * remove status
    
    * center header
    
    * add section headers
    
    * remove col values
    
    * spacing
    
    * add fields
    
    * remove contributor
    
    * s
    
    * success toast
    
    * include contact dertails

--- src/components/PoliticianEditor.vue
+++ src/components/PoliticianEditor.vue
@@ -17,7 +17,7 @@
     <el-form v-else v-on:submit.prevent="onSubmit" :rules="rules" label-position="left" label-width="100px" ref="form" :model="politician">
         <el-row >
 
-        <el-col :xs="24" :sm="12" >
+        <el-col>
               <el-form-item label="Live" prop="live">
             <el-select v-model="politician.live" placeholder="Select">
               <el-option
@@ -30,33 +30,50 @@
               </el-form-item>
           </el-col>
 
-          <el-col :xs="24" :sm="12" >
-              <el-form-item label="Status" prop="stats">
-            <el-input type="text" placeholder="enter status" v-model="politician.status"></el-input>
-              </el-form-item>
+          <el-col>
+            <h1>Main Information</h1>
           </el-col>
 
-          <el-col :xs="24" :sm="24" >
+          <el-col>
               <el-form-item label="Name" prop="name">
-            <el-input type="text" placeholder="enter name" v-model="politician.name"></el-input>
+                <el-input type="text" placeholder="enter name" v-model="politician.name"></el-input>
               </el-form-item>
           </el-col>
 
-          <el-col :xs="24" :sm="24" >
+          <el-col>
               <el-form-item label="Primary Position" prop="primary_position">
-            <el-input type="text" placeholder="enter primary position" v-model="politician.primary_position"></el-input>
+                <el-input type="text" placeholder="enter primary position" v-model="politician.primary_position"></el-input>
               </el-form-item>
           </el-col>
 
-          <el-col :xs="24" :sm="24" >
+          <el-col>
               <el-form-item label="Brief" prop="brief">
-            <el-input type="textarea" placeholder="enter brief" v-model="politician.brief"></el-input>
+                <el-input type="textarea" placeholder="enter brief" v-model="politician.brief"></el-input>
               </el-form-item>
           </el-col>
 
-          <el-col :xs="24" :sm="12" >
+          <el-col>
               <el-form-item label="Profile Image" prop="profile_image">
-            <el-input type="text" placeholder="enter profile image link" v-model="politician.profile_image"></el-input>
+                <el-input type="text" placeholder="enter profile image link" v-model="politician.profile_image"></el-input>
+              </el-form-item>
+          </el-col>
+
+          <el-col>
+            <h1>Contact Details</h1>
+          </el-col>
+
+          <el-col>
+              <el-form-item label="Email" prop="email">
+                <el-input type="text" placeholder="enter email" v-model="politician.contact_details.email"></el-input>
+              </el-form-item>
+              <el-form-item label="Phone number" prop="phone_number">
+                <el-input type="text" placeholder="enter phone number" v-model="politician.contact_details.phone_number"></el-input>
+              </el-form-item>
+              <el-form-item label="Facebook Link" prop="facebook_url">
+                <el-input type="text" placeholder="enter facebook url" v-model="politician.contact_details.facebook_url"></el-input>
+              </el-form-item>
+              <el-form-item label="Twitter Link" prop="twitter_url">
+                <el-input type="text" placeholder="enter twitter url" v-model="politician.contact_details.twitter_url"></el-input>
               </el-form-item>
           </el-col>
 
@@ -81,25 +98,20 @@ export default {
       mode: '',
       liveOptions: [{ label: 'true', value: true }, { label: 'false', value: false }],
       politician: {
-        contributor_id: this.$store.state.user.id
+        contact_details: {}
       },
       rules: {
+        email: [{ type: 'string', trigger: 'blur' }],
+        phone_number: [{ type: 'string', trigger: 'blur' }],
+        facebook_url: [{ type: 'url', trigger: 'blur' }],
+        twitter_url: [{ type: 'url', trigger: 'blur' }],
+        name: [{ required: true, message: 'Please input politician name', trigger: 'blur' }],
         live: [{ required: true, type: 'boolean', message: 'Please select whether politician is live.', trigger: 'blur' }],
         name: [{ required: true, message: 'Please input politician name', trigger: 'blur' }],
-        // contributor_id: [{ required: true, type: 'text', message: 'contributor_id is required', trigger: 'blur' }],
         primary_position: [{ required: true, message: 'primary position is required', trigger: 'blur' }],
         brief: [{ required: true, message: 'brief is required', trigger: 'blur' }],
         status: [{ required: true, message: 'Please enter status', trigger: 'blur' }],
         profile_image: [{ required: true, type: 'url', message: 'Please indicate the profile image url', trigger: 'blur' }]
-      }
-    }
-  },
-  computed: {
-    contributor: function () {
-      if (this.contributors.length > 0) {
-        const contributor = this.contributors.find(contributor => contributor.id === this.politician.contributor_id)
-        if (!contributor) return
-        return contributor.name + ' - ' + contributor.email
       }
     }
   },
@@ -110,6 +122,9 @@ export default {
         this.appStatus = 'loading'
         const id = this.$route.path.split('/').slice(-2)[0]
         const politician = await getPolitician(id)
+        if (!politician.contact_details) {
+          politician.contact_details = {}
+        }
         this.politician = politician
         this.appStatus = ''
       } else {
@@ -136,23 +151,26 @@ export default {
         this.$toast.warning('There seems to be an error with validation.', 'Oops', { position: 'topRight' })
       }
     },
+    displaySuccessToast () {
+      const verb = this.mode === 'edit' ? 'updated' : 'submitted'
+      this.$toast.success(`politician ${verb}`, 'Success', { position: 'topRight' })
+    },
     async submitPolitician (politician) {
       try {
         if (this.mode === 'new') {
           const res = await postPolitician(politician)
-          if (res.id) {
-            this.appStatus = 'submitted'
-          } else {
-            return alert(res.response.data)
+          if (!res.id) {
+            throw res.response.data
           }
         } else if (this.mode === 'edit') {
           const res = await updatePolitician(politician)
           if (res !== '') {
-            return alert(res.response.data)
-          } else {
-            this.appStatus = 'submitted'
+            throw res.response.data
           }
         }
+          this.appStatus = 'submitted'
+          this.displaySuccessToast()
+
       } catch (e) {
         console.error(e)
         this.appStatus = 'error'
@@ -170,7 +188,8 @@ export default {
 }
 
 .PoliticianEditor_header {
-  text-transform: capitalize
+  text-transform: capitalize;
+  text-align: center
 }
 
 </style>
