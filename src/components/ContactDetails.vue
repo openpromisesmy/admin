166449bf56febc96567b9407b0a7774342da4919
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-14 04:00:53 +0800

    create component

---
+++ src/components/ContactDetails.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <p><b>Email</b> {{ email }} </p>
+    <p><b>Phone Number</b> {{ phone_number }} </p>
+    <p><b>Facebook</b> {{ facebook_url }} </p>
+    <p><b>Twitter</b> {{ twitter_url }} </p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    email: {
+      type: String
+    },
+    facebook_url: {
+      type: String
+    },
+    phone_number: {
+      type: String
+    },
+    twitter_url: {
+      type: String
+    }
+  }
+}
+</script>
