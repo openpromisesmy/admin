81911d834f7dca14e3bdb4df92d1675bba38a5a1
Nazreen Mohamad <nxmohamad@gmail.com>
2019-01-14 03:28:45 +0800

    remove col values

--- src/components/PoliticianEditor.vue
+++ src/components/PoliticianEditor.vue
@@ -17,7 +17,7 @@
     <el-form v-else v-on:submit.prevent="onSubmit" :rules="rules" label-position="left" label-width="100px" ref="form" :model="politician">
         <el-row >
 
-        <el-col :xs="24" :sm="12" >
+        <el-col>
               <el-form-item label="Live" prop="live">
             <el-select v-model="politician.live" placeholder="Select">
               <el-option
@@ -34,25 +34,25 @@
             <h1>Main Information</h1>
           </el-col>
 
-          <el-col :xs="24" :sm="24" >
+          <el-col>
               <el-form-item label="Name" prop="name">
             <el-input type="text" placeholder="enter name" v-model="politician.name"></el-input>
               </el-form-item>
           </el-col>
 
-          <el-col :xs="24" :sm="24" >
+          <el-col>
               <el-form-item label="Primary Position" prop="primary_position">
             <el-input type="text" placeholder="enter primary position" v-model="politician.primary_position"></el-input>
               </el-form-item>
           </el-col>
 
-          <el-col :xs="24" :sm="24" >
+          <el-col>
               <el-form-item label="Brief" prop="brief">
             <el-input type="textarea" placeholder="enter brief" v-model="politician.brief"></el-input>
               </el-form-item>
           </el-col>
 
-          <el-col :xs="24" :sm="12" >
+          <el-col>
               <el-form-item label="Profile Image" prop="profile_image">
             <el-input type="text" placeholder="enter profile image link" v-model="politician.profile_image"></el-input>
               </el-form-item>
