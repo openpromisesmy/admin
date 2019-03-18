dae00660ca0a5316f71514ea97968a1f3231c592
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-18 21:36:30 +0800

    remove empty line

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,54 +1,52 @@
 <template>
   <div class="container">
     <template v-if="appStatus === 'loading'">
-      <LoadingSpinner />
+      <LoadingSpinner/>
     </template>
     <template v-else>
       <h1>{{ list.title }}</h1>
-    <promises-table :promises="promises" exclude="[source_name, politician_name]" />
-
+      <promises-table :promises="promises" exclude="[source_name, politician_name]"/>
     </template>
   </div>
 </template>
 
 <script>
-import { getList, getPromise } from '@/api'
-import PromisesTable from '@/components/PromisesTable'
-import LoadingSpinner from '@/components//LoadingSpinner'
-import queryString from 'query-string'
+import { getList, getPromise } from "@/api";
+import PromisesTable from "@/components/PromisesTable";
+import LoadingSpinner from "@/components//LoadingSpinner";
+import queryString from "query-string";
 
 export default {
-  name: 'List',
-  data () {
+  name: "List",
+  data() {
     return {
-      appStatus: '',
+      appStatus: "",
       list: {},
       promises: []
-    }
+    };
   },
   components: { LoadingSpinner, PromisesTable },
-  async created () {
+  async created() {
     try {
-      this.appStatus = 'loading'
-      this.list = await this.getListHandler(this.$route.params.id)
+      this.appStatus = "loading";
+      this.list = await this.getListHandler(this.$route.params.id);
       this.list.promise_ids.forEach(async promise_id => {
-        this.promises.push(await getPromise(promise_id))
-      })
-      this.appStatus = ''
+        this.promises.push(await getPromise(promise_id));
+      });
+      this.appStatus = "";
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   },
   methods: {
-    async getListHandler (id) {
-      return await getList(id)
+    async getListHandler(id) {
+      return await getList(id);
     },
-    async getPromiseHandler (id) {
-      return await getPromise(id)
+    async getPromiseHandler(id) {
+      return await getPromise(id);
     }
   }
-}
-
+};
 </script>
 
 <style scoped>
