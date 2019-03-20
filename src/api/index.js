a87952a2dc7358fbc9fa0ef2555cb684b5d36eff
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-21 01:07:39 +0800

    extract mock

--- src/api/index.js
+++ src/api/index.js
@@ -8,6 +8,8 @@ import {
   googleSignIn,
   googleLogout
 } from './google'
+import mocks from '../mocks'
+const mockLists = mocks.lists
 
 const PROMISES_PATH = '/promises/'
 const PROMISE_UPDATES_PATH = '/promiseUpdates/'
@@ -48,34 +50,6 @@ const postPromiseUpdate = data => postSomething(PROMISE_UPDATES_PATH, data)
 const updatePromiseUpdate = data => updateSomething(`${PROMISE_UPDATES_PATH}${data.id}`, data)
 
 const getGeneralStats = () => getSomething('/stats/general_stats')
-
-const mockLists = [
-  {
-    id: 'm54ertdfgcv',
-    live: false,
-    title: 'Student Aids',
-    contributor_id: '-LAEDTAC6HQeWYrV3atG',
-    created_at: "2019-03-17T18:35:05.441Z",
-    updated_at: "2019-03-17T18:35:05.441Z",
-    promise_ids: [
-      'CeX1MLvSq7IVx3nI1CgI',
-      'U5zwNgRbdu0NZRjFLoBF',
-      'o3lyMjqZrCALP6HSUZFS'
-    ]
-  },
-  {
-    id: 'ytughjbsdf8t7yiu',
-    live: true,
-    title: 'Broadband service in Malaysia',
-    contributor_id: '-LAEDTAC6HQeWYrV3atG',
-    created_at: "2019-03-17T18:35:05.441Z",
-    updated_at: "2019-03-17T18:35:05.441Z",
-    promise_ids: [
-      'XjGLsRtJ9spk5VeBidDl',
-      'bGvbmCPqIcpWQIZAp92C'
-    ]
-  }
-]
 
 // TODO: replace with real
 const listLists = () => mockLists
