9758bfcbbc49b4e9cf144502dc3088389b704ecd
Nazreen Mohamad <nxmohamad@gmail.com>
2019-03-21 01:05:52 +0800

    update mock

--- src/api/index.js
+++ src/api/index.js
@@ -52,7 +52,11 @@ const getGeneralStats = () => getSomething('/stats/general_stats')
 const mockLists = [
   {
     id: 'm54ertdfgcv',
+    live: false,
     title: 'Student Aids',
+    contributor_id: '-LAEDTAC6HQeWYrV3atG',
+    created_at: "2019-03-17T18:35:05.441Z",
+    updated_at: "2019-03-17T18:35:05.441Z",
     promise_ids: [
       'CeX1MLvSq7IVx3nI1CgI',
       'U5zwNgRbdu0NZRjFLoBF',
@@ -61,7 +65,11 @@ const mockLists = [
   },
   {
     id: 'ytughjbsdf8t7yiu',
+    live: true,
     title: 'Broadband service in Malaysia',
+    contributor_id: '-LAEDTAC6HQeWYrV3atG',
+    created_at: "2019-03-17T18:35:05.441Z",
+    updated_at: "2019-03-17T18:35:05.441Z",
     promise_ids: [
       'XjGLsRtJ9spk5VeBidDl',
       'bGvbmCPqIcpWQIZAp92C'
