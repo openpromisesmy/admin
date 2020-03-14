91a6797fdb0c5d55d152baab2e667fc2d84443f4
Nazreen <nxmohamad@gmail.com>
2020-03-14 09:52:46 +0000

    hide contributors in navbar (#187)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,16 @@
 <template>
   <el-header id="navbar">
-      <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
-        <el-menu-item v-for="(item, index) in navigation" :key="index" v-bind:index="String(index)">
-          <router-link v-bind:to="item.url">{{ item.text }}</router-link>
-        </el-menu-item>
-        <el-menu-item index="4" id="account">
-          <router-link to="/account">
-            {{ this.$store.state.user.authenticated ? 'Account' : 'Login' }}
-          </router-link>
-        </el-menu-item>
-        <!-- move below to inside account page -->
-        <el-menu-item index="5" v-if="this.$store.state.user.authenticated">
-            <el-button type="info" v-on:click="googleLogoutHandler">
-              Logout
-            </el-button>
-        </el-menu-item>
+    <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
+      <el-menu-item v-for="(item, index) in navigation" :key="index" v-bind:index="String(index)">
+        <router-link v-bind:to="item.url">{{ item.text }}</router-link>
+      </el-menu-item>
+      <el-menu-item index="4" id="account">
+        <router-link to="/account">{{ this.$store.state.user.authenticated ? 'Account' : 'Login' }}</router-link>
+      </el-menu-item>
+      <!-- move below to inside account page -->
+      <el-menu-item index="5" v-if="this.$store.state.user.authenticated">
+        <el-button type="info" v-on:click="googleLogoutHandler">Logout</el-button>
+      </el-menu-item>
     </el-menu>
   </el-header>
 </template>
@@ -33,7 +29,7 @@ export default {
         { text: 'Lists', url: '/lists' },
         // { text: 'Documents', url: '/documents' },
         { text: 'Politicians', url: '/politicians' },
-        { text: 'Contributors', url: '/contributors' },
+        // { text: 'Contributors', url: '/contributors' },
         { text: 'Stats', url: '/stats' }
       ],
       authenticated: this.$store.state.user.authenticated,
@@ -60,19 +56,19 @@ export default {
 <style scoped>
 a {
   text-decoration: none;
-  font-size: 1rem
+  font-size: 1rem;
 }
 
 #submit {
-  float:right
+  float: right;
 }
 
 @media only screen and (max-width: 600px) {
-    .el-header {
-      margin-bottom: 30px;
-    }
-    .el-menu-item {
-      padding: 0 10px;
-    }
+  .el-header {
+    margin-bottom: 30px;
+  }
+  .el-menu-item {
+    padding: 0 10px;
+  }
 }
 </style>
